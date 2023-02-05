fab7cede7963e6d5cac86d26b62b3c16027dbb4e
enzo jakobasch <enzo.jakobasch@diderot.education>
2023-02-05 11:52:52 +0100

    details

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: "/.env" });
 
-const ALCHEMY_API_KEY = ALCHEMY_KEY;
-const GOERLI_PRIVATE_KEY = GOERLI_KEY;
+const { ALCHEMY_API_KEY, GOERLI_PRIVATE_KEY } = process.env;
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -9,7 +9,7 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [GOERLI_PRIVATE_KEY]
     }
   }
